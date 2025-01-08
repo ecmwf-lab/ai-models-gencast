d8c63dc870084d7ba02ddfffc1f477d9eda32073
Harrison Cook <harrison.cook@ecmwf.int>
2025-01-08 14:27:07 +0000

    Fix mask

--- src/ai_models_gencast/model.py
+++ src/ai_models_gencast/model.py
@@ -283,7 +283,7 @@ class GenCast(Model):
             training_xarray["land_sea_mask"].values = np.load(get_path(self.download_masks[1]))
             training_xarray["geopotential_at_surface"].values = np.load(get_path(self.download_masks[0]))
 
-            sst_mask = np.load(get_path(self.download_masks[2])) is False
+            sst_mask = np.load(get_path(self.download_masks[2])) == False  # noqa: E712
             training_xarray["sea_surface_temperature"] = training_xarray["sea_surface_temperature"].where(sst_mask)
 
             gc.collect()
